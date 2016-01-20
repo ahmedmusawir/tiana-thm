e6cc15063878891668c0d9834e023dcb40d02901
Da Moose <ahmed.musawir@hotmail.com>
2016-01-20 13:59:53 +0800

    2nd Commit: Header and Header form pretty much done. Working on sidebar

--- header.php
+++ header.php
@@ -26,9 +26,9 @@
 
 	<header id="masthead" class="site-header" role="banner">
 	
-		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<nav class="navbar navbar-inverse-not navbar-fixed-top-not" role="navigation">
 	
-  		  <div class="container">
+  		  <div class="container-fluid">
 
 		    <!-- Brand and toggle get grouped for better mobile display -->
 		    <div class="navbar-header">
@@ -38,7 +38,7 @@
 		            <span class="icon-bar"></span>
 		            <span class="icon-bar"></span>
 		        </button>
-		    <a class="navbar-brand hidden-lg" href="#"><img src="<?php echo $siteLogo_mobile;  ?>" alt=""></a>
+		    <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="logo" src="http://tiana/wp-content/uploads/2016/01/TianaGustafsonLogo.png" alt=""></a>
 		    </div>
 
 		    <!-- Collect the nav links, forms, and other content for toggling -->
@@ -69,23 +69,46 @@
 
 	
 	<header id="pg-header">
-		<img class="img-responsive" src="http://tiana/wp-content/uploads/2016/01/tiana-header-img-1-noise-light.jpg" alt="">
+		<img class="img-responsive" src="http://tiana/wp-content/uploads/2016/01/tiana-header-img-1-noise-light.jpg" alt="tiana img">
 		<!-- <img class="img-responsive" src="http://tiana/wp-content/uploads/2016/01/tiana-header-img-1-noise.jpg" alt=""> -->
 		<!-- <img class="img-responsive" src="http://tiana/wp-content/uploads/2016/01/tiana-header-img-1-2.jpg" alt=""> -->
+		<div class="header-text-container">
+			<article class="header-text-box">
+				<h3>Get Paid to Be You!</h3>
+				<h4>
+					I can show you how to Utilize<br>
+					the POWER of Social Media and<br>
+					FREE Your AUTHENTIC Self to <br>
+					Sell or Promote Any Service <br>
+					or Product
+				</h4>
+			</article>			
+		</div>
 		
 	</header>
 	<section class="header-form-section">
+
 		<div class="header-form-container container">
-			<form class="form-inline pull-right" role="form">
-			  <div class="form-group">
-			    <input type="email" class="form-control" id="email" placeholder="First Name">
-			  </div>
-			  <div class="form-group">
-			    <input type="password" class="form-control" id="pwd" placeholder="Email">
-			  </div>
-			  
-			  <button type="submit" class="btn btn-default">Submit</button>
-			</form>
+			<div class="col-md-6">
+				<h3 class="form-section-text pull-right">
+				Free Your Body, Life & Business.<br>
+				Get Your FREE Inspirational Workbook Now ...
+				</h3>
+			</div>
+			<div class="col-md-6">
+				<form class="form-inline header-form" role="form">
+				  <div class="form-group">
+				    <input type="email" class="form-control" id="email" placeholder="First Name">
+				  </div>
+				  <div class="form-group">
+				    <input type="password" class="form-control" id="pwd" placeholder="Email">
+				  </div>
+				  
+				  <button type="submit" class="btn btn-default header-from-submit">Get<br> it!</button>
+				</form>
+			</div>
+
+			
 		</div>
 	</section>
 
